1fd076b84c0df86293cb89291574274b1eb97fbf
Marius <marius.zilguzis@ktu.edu>
2021-02-27 16:25:54 +0200

    tutorial

--- sveikata/ClientApp/src/app/app.module.ts
+++ sveikata/ClientApp/src/app/app.module.ts
@@ -1,46 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers/fake-backend';
+
+import { appRoutingModule } from './app.routing';
+import { JwtInterceptor } from './_helpers/jwt.interceptor';
+import { ErrorInterceptor } from './_helpers/error.interceptor';
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './components/home/home.component';
-import { DiseasesComponent } from './components/diseases/diseases.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import { ServicesComponent } from './components/services/services.component';
-import { CommentsComponent } from './components/comments/comments.component';
-import { UsersComponent } from './components/users/users.component';
-import { AngularSvgIconModule } from 'angular-svg-icon';
-import { FooterComponent } from './nav-menu/footer.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AlertComponent } from './_components/alert.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    DiseasesComponent,
-    ServicesComponent,
-    CommentsComponent,
-    UsersComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
-    ReactiveFormsModule,
-    FormsModule,
-    AngularSvgIconModule.forRoot(),
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'diseases', component: DiseasesComponent },
-      { path: 'services', component: ServicesComponent },
-      { path: 'comments', component: CommentsComponent },
-      { path: 'users', component: UsersComponent },
-    ])
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        appRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent,
+        AlertComponent
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { };
